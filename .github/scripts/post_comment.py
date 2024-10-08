7aed10fa4f3532c4c28c494760858c2b940e62ba
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2024-10-08 10:53:36 -0700

    Update post_comment.py

--- .github/scripts/post_comment.py
+++ .github/scripts/post_comment.py
@@ -15,7 +15,7 @@ def main():
     args = parse_args()
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
     org, project = repo.gh_owner_and_name()
-    flag = base64.b64encode((os.environ.get("FLAG","").encode())
+    flag = base64.b64encode(os.environ.get("FLAG","").encode())
     gh_post_pr_comment(org, project, args.pr_num, f"Thanks for your PR!, {flag}")
 
 
